75f27157ea3ed938f30c3a463209158c6b5dc1c4
erbuckner <erbuckner@gmail.com>
2021-10-10 17:48:32 -0400

    * JobField.cs: Created base class JobField
    * Job.cs:
    
    * Program.cs: Created

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -6,7 +6,7 @@ namespace TechJobsOO
         public int Id { get; }
         private static int nextId = 1;
 
-        public string Name { get; set; }
+        public string Value { get; set; }
         public Employer EmployerName { get; set; }
         public Location EmployerLocation { get; set; }
         public PositionType JobType { get; set; }
@@ -20,9 +20,9 @@ namespace TechJobsOO
             nextId++;
         }
 
-        public Job (string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCompetency) : this()
+        public Job (string value, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCompetency) : this()
         {
-            Name = name;
+            Value = value;
             EmployerName = employerName;
             EmployerLocation = employerLocation;
             JobType = jobType;
@@ -44,12 +44,12 @@ namespace TechJobsOO
 
         public override string ToString()
         {
-            return $"r/n/ ID: {this.Id} /n " +
-                $"Name: {this.Name} /n " +
-                $"Employer: {this.EmployerName.Value} /n" +
-                $"Location: {this.EmployerLocation.Value} /n" +
-                $"Position Type: {this.JobType.Value} /n" +
-                $"Core Competency: {this.JobCoreCompetency.Value} r/n/"; 
+            return $"\nID: {this.Id} \n" +
+                $"Name: {this.Value} \n" +
+                $"Employer: {this.EmployerName.Value} \n" +
+                $"Location: {this.EmployerLocation.Value} \n" +
+                $"Position Type: {this.JobType.Value} \n" +
+                $"Core Competency: {this.JobCoreCompetency.Value} \n"; 
         }
 
     }
