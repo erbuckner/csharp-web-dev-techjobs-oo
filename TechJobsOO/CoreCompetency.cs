ca8ff60385c78de3ad00846aa3574d9004f38187
erbuckner <erbuckner@gmail.com>
2021-10-10 18:02:36 -0400

    Extended base class JobField Deleted shared code

--- TechJobsOO/CoreCompetency.cs
+++ TechJobsOO/CoreCompetency.cs
@@ -1,41 +1,13 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class CoreCompetency
+    public class CoreCompetency : JobField
     {
-        // TODO: Change the fields to auto-implemented properties.
-        // DONE!
-        public int Id { get; set; }
-        private static int nextId = 1;
-        public string Value { get; set; }
 
-
-        public CoreCompetency()
+        public CoreCompetency(string value) : base(value)
         {
-            Id = nextId;
-            nextId++;
         }
 
-        public CoreCompetency(string value) : this()
-        {
-            Value = value;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is CoreCompetency competency &&
-                   Id == competency.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
     }
 }
 
